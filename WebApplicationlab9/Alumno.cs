a2bf6e6cefac119ba84d470bdafff4837089a7c3
aemendez <aemendez@umes.edu.gt>
2024-04-15 08:33:55 -0600

    Agregar archivos de proyecto.

---
+++ WebApplicationlab9/Alumno.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplicationlab9
+{
+    public class Alumno
+    {
+        int dpi;
+        string nombre;
+        string direccion;
+
+        public int Dpi { get => dpi; set => dpi = value; }
+        public string Nombre { get => nombre; set => nombre = value; }
+        public string Direccion { get => direccion; set => direccion = value; }
+    }
+}
